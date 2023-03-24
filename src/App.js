c211d21e125ad508b88ebb68e9742721abdbb605
Jade-Jones-dev <100792140+Jade-Jones-dev@users.noreply.github.com>
2023-03-24 08:48:36 +0000

    routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import {Routes, Route} from 'react-router-dom'
 import './App.css';
+
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About/>} />
+      <Route path='/projects' element={<Projects />} />
+      </Routes>
     </div>
   );
 }
