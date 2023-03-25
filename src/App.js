8799b86b9fb8734f4f77162160d76181328e2530
Jade-Jones-dev <100792140+Jade-Jones-dev@users.noreply.github.com>
2023-03-25 18:53:13 +0000

    added dark theme

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import {Routes, Route} from 'react-router-dom'
 import './App.css';
-
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+
+
+
 function App() {
   return (
-    <div className="App">
-      <Routes>
+    <ThemeProvider theme={darkTheme}>
+    <CssBaseline />
+    <main>This app is using the dark mode</main>
+    <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/about' element={<About/>} />
       <Route path='/projects' element={<Projects />} />
       </Routes>
-    </div>
-  );
-}
+  </ThemeProvider>
+
+   
+)}
 
 export default App;
